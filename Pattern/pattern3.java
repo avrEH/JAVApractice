b69e39f991cbe06fd87c93cad944fb11d9622ae5
avrEH <avrrai3105@gmail.com>
2024-07-04 00:56:48 +0530

    New pattern added

---
+++ Pattern/pattern3.java
@@ -0,0 +1,21 @@
+package Pattern;
+class pattern3{
+    public static void main(String[] args) {
+        int n=5;
+        for (int i =1;i<= n ; i ++)
+        {
+            for( int j = n;j >=i;j--)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+
+        }
+    }
+}
+//OUTPUT
+// * * * * * 
+// * * * * 
+// * * * 
+// * * 
+// * 
