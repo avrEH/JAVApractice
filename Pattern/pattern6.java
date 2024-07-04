d6af36638ea80910823fac8ac61387c04a26d9a8
avrEH <avrrai3105@gmail.com>
2024-07-04 15:40:59 +0530

    New pattern addded

---
+++ Pattern/pattern6.java
@@ -0,0 +1,24 @@
+package Pattern;
+class pattern6
+{    
+    public static void main ( String[] args)
+    {
+        int n =  5;
+    for ( int i = 1 ; i <= n ; i ++ )
+    {
+        int spa=n-i;
+        for (int space = 1 ;space<=spa;space++)
+        {
+            System.out.print(" ");
+        }
+        for ( int j =  1; j <=i ;j++)
+        {
+            System.out.print("*");
+
+        }
+        System.out.println();
+    }
+
+        }
+    
+}
