75dca6b59f79d47e767cb768304d5ce53ec99486
avrEH <avrrai3105@gmail.com>
2024-07-07 21:16:48 +0530

    new pattern added

--- Pattern/pattern10.java
+++ Pattern/pattern10.java
@@ -1,33 +1,45 @@
 package Pattern;
-class pattern10
-{
+class pattern10 {
     public static void main(String[] args) {
         
-        int n = 5;
-        int c;
-        for (int i = 1;i<=2*n;i++)
-        {
-            if (i>n)
-            c=2*n-i;
-            else
-            c=i;
+        int n = 5; // The number of rows for the upper half of the pattern
+        int c; // Variable to hold the current row number for the pattern
 
-            for (int space = 0; space <= n- c; space++) {
+        // Loop for each row from 1 to 2n (total number of rows)
+        for (int i = 1; i <= 2*n; i++) {
+            if (i > n)
+                c = 2*n - i; // Adjust row number for the lower half of the pattern
+            else
+                c = i; // Row number for the upper half of the pattern
+
+            // Print leading spaces
+            for (int space = 0; space <= n - c; space++) {
                 System.out.print("  ");
             }
 
-            // Print decreasing part of the pattern
+            // Print descending numbers from c to 1
             for (int j = c; j >= 1; j--) {
                 System.out.print(j + " ");
             }
 
-            // Print increasing part of the pattern
+            // Print ascending numbers from 2 to c
             for (int j = 2; j <= c; j++) {
                 System.out.print(j + " ");
             }
 
-            // Move to the next line after each row
-            System.out.println();  
-        }}
+            // Move to the next line
+            System.out.println();
+        }
     }
+}//     OUTPUT
+//           1 
+//         2 1 2
+//       3 2 1 2 3
+//     4 3 2 1 2 3 4
+//   5 4 3 2 1 2 3 4 5
+//     4 3 2 1 2 3 4
+//       3 2 1 2 3
+//         2 1 2
+//           1
+
 
