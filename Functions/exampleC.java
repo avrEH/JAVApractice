419170f8a49eac7b9e074fec0004ee8b669923cc
avrEH <avrrai3105@gmail.com>
2024-07-20 16:12:03 +0530

    example

---
+++ Functions/exampleC.java
@@ -0,0 +1,12 @@
+class exampleC
+{
+    public static void main(String[] args) {
+        String message = greet();
+        System.out.println(message);
+
+    }
+    static String greet(){
+        String greeting = "how are you";
+        return greeting ;
+    }
+}
