b081586b947b2f59474ecb60adcea97375eb140e
avrEH <avrrai3105@gmail.com>
2024-09-09 14:04:39 +0530

    New program added

--- insertionsort.java
+++ insertionsort.java
@@ -1,6 +1,9 @@
-package Arrays;
-import java.util.*;
-public class selectionsort {
+import java.util.Scanner;
+
+/**
+ * insertionsort
+ */
+public class insertionsort {
 
     public static void main(String[] args) {
         System.out.println("Enter the no of element you want in array");
@@ -11,24 +14,20 @@ public class selectionsort {
         for(int i=0;i<arr.length;i++){
             arr[i]=sc.nextInt();
         }
-        for(int i = 0;i<arr.length;i++){
-            int last =arr.length-i-1;
-            int maxIndex= getMaxIndex(arr,0,last);//get max is declared below
-            swap(arr, maxIndex, last);//swap is declared below
+        for (int i = 0;i<arr.length-1;i++){
+            for (int j=i+1;j>0;j--){
+                if(arr[j]<arr[j-1]){
+                    swap(arr, j, j-1);
+                }
+                else{
+                    break;
 
+                }
+            }
         }
         for(int i=0;i<arr.length;i++){
             System.out.print(arr[i]+" ");
         }
-        
-    }
-    static int getMaxIndex(int[]arr , int start,int end){
-        int max= start ;
-        for (int i =start;i<=end;i++ ){
-            if (arr[max]<arr[i]){
-                max=i;
-            }
-        }return max;
 
     }
     static void swap(int []arr,int first,int second){
