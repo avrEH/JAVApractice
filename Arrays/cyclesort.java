4f3c613d2d4df387b52f1c8fd32320e0e70679fe
avrEH <avrrai3105@gmail.com>
2024-09-09 14:37:05 +0530

    new cycle sort added

---
+++ Arrays/cyclesort.java
@@ -0,0 +1,40 @@
+
+//only can be usend when given arrayy is in the form of 1 to N
+
+import java.util.Scanner;
+
+public class cyclesort {
+ public static void main(String[] args) {
+    
+        System.out.println("Enter the no of element you want in array");
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        System.out.println("Enter element in array");
+        int arr[]= new int[x];
+        for(int i=0;i<arr.length;i++){
+            arr[i]=sc.nextInt();
+        }
+        int i =0;
+        while(i<arr.length){
+            int correct=arr[i]-1;
+            if(arr[i]!=arr[correct]){
+                swap(arr, i, correct);
+            }
+            else{
+                i++;
+            }
+
+        }
+        for(int j=0;j<arr.length;j++){
+            System.out.print(arr[j]+" ");
+        }
+
+    
+ }
+ static void swap(int []arr,int first,int second){
+    int temp=arr[first];
+    arr[first]=arr[second];
+    arr[second]=temp;
+}
+    
+}
